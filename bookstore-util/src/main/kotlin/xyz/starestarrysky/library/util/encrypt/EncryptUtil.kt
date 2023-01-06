60736588a54c29dadef48e1a9c46f3a4e6e7a668
zxy <zcbtyz@qq.com>
2023-01-06 16:49:41 +0800

    open a constant.

--- bookstore-util/src/main/kotlin/xyz/starestarrysky/library/util/encrypt/EncryptUtil.kt
+++ bookstore-util/src/main/kotlin/xyz/starestarrysky/library/util/encrypt/EncryptUtil.kt
@@ -3,23 +3,25 @@ package xyz.starestarrysky.library.util.encrypt
 import java.util.Base64
 
 object EncryptUtil {
+    const val contentSplit = "||||"
+
     fun encode(content: String, publicKey: String): String {
         var all = content.toByteArray()
         var result = ""
         val key = Base64.getDecoder().decode(publicKey)
         while (all.size > 117) {
             val block = all.sliceArray(0..116)
-            result += RSACrypto.encrypt(block, key) + "||||"
+            result += RSACrypto.encrypt(block, key) + contentSplit
             all = all.sliceArray(IntRange(117,all.size - 1))
         }
         return result + RSACrypto.encrypt(all, key)
     }
 
-    fun decode(content: String, rsaPrivate: String): String {
-        val blocks = content.split("||||")
+    fun decode(content: String, privateKey: String): String {
+        val blocks = content.split(contentSplit)
         val all = mutableListOf<Byte>()
         blocks.forEach {
-            val block = RSACrypto.decryptToBytes(it, rsaPrivate)
+            val block = RSACrypto.decryptToBytes(it, privateKey)
             all.addAll(block.toTypedArray())
         }
         return String(all.toByteArray())
